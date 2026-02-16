b1e94509954a582d250bd269d3ade635e96d73c5
Viniathie12363 <Viniathie12363@gmail.com>
2026-02-16 20:51:29 -0300

    Add files via upload

---
+++ frutas.py
@@ -0,0 +1,27 @@
+#%%
+
+import pandas as pd
+
+df = pd.read_excel("data/dados_frutas.xlsx")
+
+
+
+
+# %%
+
+from sklearn import tree
+
+arvore = tree.DecisionTreeClassifier(random_state=42)
+# %%
+
+y = df['Fruta']
+
+caracteristicas = ['Arredondada','Suculenta', 'Vermelha','Doce']
+
+X = df[caracteristicas]
+
+# %%
+arvore.fit(X,y)
+# %%
+arvore.predict([[0,1,0,0]])
+# %%
